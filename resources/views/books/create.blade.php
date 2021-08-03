ac8f17b8ac8a3f8763fdb3bcb46a83d07828f60c
Tenoga <castroherreradavid@gmail.com>
2021-08-03 11:31:26 -0500

    create books

--- resources/views/books/create.blade.php
+++ resources/views/books/create.blade.php
@@ -7,10 +7,18 @@
                 <form action="/books" method="POST">
                 @csrf
                     <div class="form-group">
-                        <label for="name">Name</label>
-                        <input type="text" class="form-control" id="name" name="name" placeholder="Type your name">
-                        <label for="name">UserName</label>
-                        <input type="text" class="form-control" id="userName" name="userName" placeholder="Type your User Name">                        
+                        <label for="title">Book Name</label>
+                        <input type="text" class="form-control" id="bookName" name="bookName" placeholder="Type the book name">
+                        <label for="autor">Author</label>
+                        <input type="text" class="form-control" id="userName" name="userName" placeholder="Type the author">  
+                        <label for="genre">Genre</label>
+                        <input type="text" class="form-control" id="Genre" name="Genre" placeholder="Select the genre">  
+                        <label for="edition">Edition</label>
+                        <input type="text" class="form-control" id="edition" name="edition" placeholder="Select the edition">
+                        <label for="publication_year">Publication year</label>
+                        <input type="text" class="form-control" id="publication_year" name="publication_year" placeholder="Select publication year"> 
+                        <label for="status">Status</label>
+                        <input type="text" class="form-control" id="status" name="status" placeholder="Select the status">                                             
                     </div>
                     <button type="submit" class="btn btn-warning">Submit</button>
                 </form>                
