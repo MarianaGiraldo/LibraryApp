999454ee76a98a435f7965c3358e770bf1e47002
MarianaGiraldo <mgiraldo594@misena.edu.co>
2021-08-03 09:43:18 -0500

    books

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,31 +1,26 @@
 @extends('layouts.layout')
 
 @section('content')
-        <div class="row showBooks">
+        <div class="row ">
             <div class="col">
-                <h2>Books</h2>
-                <table class="table-light hover">
-                    <tr>
-                        <th>Id</th>
-                        <th>Name</th>
-                        <th>Autor</th>
-                        <th>Genre</th>
-                        <th>Edition</th>
-                        <th>Publication year</th>
-                        <th>Status</th>
-                    </tr>
-                @foreach($books as $books)
-                    <tr>
-                        <td>{{$books->id}}</td>
-                        <td>{{$books->name}}</td>
-                        <td>{{$books->autor}}</td>
-                        <td>{{$books->genre}}</td>
-                        <td>{{$books->edition}}</td>
-                        <td>{{$books->publication_year}}</td>
-                        <td>{{$books->status}}</td>
-                    </tr>
-                @endforeach 
-                </table>
+                <h2 class="m-3">Books</h2>
+                <div class="row">
+                @foreach($books as $book)
+                    <div class="card col-4 showBooks m-4" >
+                        <img src="..." class="card-img-top" alt="Book image">
+                        <div class="card-body">
+                            <h5 class="card-title">Book title: {{$book->title}} </h5>
+                            <p class="card-text">Autor: {{$book->autor}} </p>
+                            <p class="card-text">Genre: {{$book->genre}} </p>
+                            <p class="card-text">Edition: {{$book->edition}} </p>
+                            <p class="card-text">Publication Year: {{$book->publication_year}} </p>
+                            <p class="card-text">Status: {{$book->status}} </p>
+                            <a href="#" class="btn btn-primary">View more</a>
+                            <p class="card-text text-end">{{$book->id}} </p>
+                        </div>
+                    </div>
+                @endforeach
+                </div>
             </div>
         </div>
 @endsection
