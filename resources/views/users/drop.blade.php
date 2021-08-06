e3e88c4bbf854b328da49510d53a96f53cd3ecb8
Tenoga <castroherreradavid@gmail.com>
2021-08-06 15:30:20 -0500

    drop users

--- resources/views/users/drop.blade.php
+++ resources/views/users/drop.blade.php
@@ -3,17 +3,20 @@
 @section('content')
 <div class="row m-4 ">
         <div class="col">
-            <h1>¿Delete User {{$deleteUser-> name}} ?</h1>
+            <h1>¿Delete User {{$dropUser-> firstName}} ?</h1>
         </div>
     <div class="row">
             <div class="col">
-                <form action="/users/{{deleteUser->id}}" method="POST">
+                <form action="{{ route('users.destroy'), $dropUser->id}}" method="POST" class="w-50 mx-auto formularioConfirm p-4 rounded">
                 @csrf
-                @method('delete')
-                    <button type="submit" class="btn btn-warning">Delete</button>
+                @method('DELETE')
+                <div class="formgroup row mb-3">
+                    <label for="delete" class="col col-form-label fs-3 ">Confirm if you want to delete the user. </label>
+                </div>
+                <button type="submit" class="btn btn-warning">Delete</button>
                 </form>
+                <a href="/users" class="btn btn-primary m-3" role="button">Cancel</a>
             </div>
     </div>
-    <a href="/users" class="btn btn-primary m-3" role="button">Back</a>
 </div>
 @endsection
