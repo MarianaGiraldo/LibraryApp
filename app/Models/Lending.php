8434218d1ab452f24e060117a9481ef2ba1c2f36
MarianaGiraldo <mgiraldo594@misena.edu.co>
2021-08-10 12:05:34 -0500

    Relaciones de tablas

--- app/Models/Lending.php
+++ app/Models/Lending.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Lending extends Model
 {
     use HasFactory;
+    public function lendingUser(){
+        return $this->belongsTo(User::class);
+    }
+    public function lendingBook(){
+        return $this->belongsTo(Book::class);
+    }
 }
